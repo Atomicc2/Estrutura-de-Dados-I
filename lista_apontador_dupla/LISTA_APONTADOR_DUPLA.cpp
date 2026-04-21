5b86da9d698afed617f5488059169d47a274cdf4
Anderson S. <andersonacpemp@gmail.com>
2026-04-20 22:11:42 -0300

    Adiconando lista duplamente encadeada e já adicionando também a possibilidade de leitura do arquivo

---
+++ lista_apontador_dupla/LISTA_APONTADOR_DUPLA.cpp
@@ -0,0 +1,238 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include "LISTA_APONTADOR_DUPLA.h"
+using namespace std;
+
+//--------------------------------------------
+void Cria(TLista &Lista)
+{
+    Lista.Primeiro = new NodoLista;
+    Lista.Ultimo = Lista.Primeiro;
+    Lista.Primeiro->Proximo = NULL;
+    Lista.Primeiro->Anterior = NULL;
+}
+//--------------------------------------------
+bool Vazia(TLista Lista)
+{
+    return Lista.Primeiro == Lista.Ultimo;
+}
+//--------------------------------------------
+void InsereInicio(TLista &Lista, TInfo Item)
+{
+    Apontador p = new NodoLista;
+    p->Item = Item;
+    p->Proximo = Lista.Primeiro->Proximo;
+    p->Anterior = Lista.Primeiro;
+    Lista.Primeiro->Proximo = p;
+    if (p->Proximo == NULL)
+        Lista.Ultimo = p;
+    else
+        p->Proximo->Anterior = p;
+}
+//--------------------------------------------
+void InsereFinal(TLista &Lista, TInfo Item)
+{
+    Apontador p = new NodoLista;
+    p->Item = Item;
+    p->Proximo = NULL;
+    p->Anterior = Lista.Ultimo;
+    Lista.Ultimo->Proximo = p;
+    Lista.Ultimo = p;
+}
+//--------------------------------------------
+void InserePosicao(TLista &Lista, int pos, TInfo Item)
+{
+    if (pos <= 1 || Vazia(Lista))
+    {
+        InsereInicio(Lista, Item);
+        return;
+    }
+
+    Apontador anterior = Lista.Primeiro;
+    int i = 1;
+    while (anterior->Proximo != NULL && i < pos)
+    {
+        anterior = anterior->Proximo;
+        i++;
+    }
+
+    Apontador p = new NodoLista;
+    p->Item = Item;
+    p->Proximo = anterior->Proximo;
+    p->Anterior = anterior;
+    anterior->Proximo = p;
+    if (p->Proximo == NULL)
+        Lista.Ultimo = p;
+    else
+        p->Proximo->Anterior = p;
+}
+//--------------------------------------------
+void RemoveInicio(TLista &Lista)
+{
+    if (Vazia(Lista))
+    {
+        cout << "Erro : A lista esta vazia." << endl;
+    }
+    else
+    {
+        Apontador p = Lista.Primeiro->Proximo;
+        cout << "Elemento Removido = " << p->Item.ID << endl;
+        Lista.Primeiro->Proximo = p->Proximo;
+        if (Lista.Primeiro->Proximo == NULL)
+            Lista.Ultimo = Lista.Primeiro;
+        else
+            p->Proximo->Anterior = Lista.Primeiro;
+        delete p;
+    }
+}
+//--------------------------------------------
+void RemoveFinal(TLista &Lista)
+{
+    if (Vazia(Lista))
+    {
+        cout << "Erro : A lista esta vazia." << endl;
+        return;
+    }
+
+    Apontador p = Lista.Ultimo;
+    cout << "Elemento Removido = " << p->Item.ID << endl;
+    p->Anterior->Proximo = NULL;
+    Lista.Ultimo = p->Anterior;
+    delete p;
+}
+//--------------------------------------------
+void PesquisaRemove(TLista &Lista, TInfo Item)
+{
+    if (Vazia(Lista))
+    {
+        cout << "Erro : A lista esta vazia." << endl;
+        return;
+    }
+
+    Apontador anterior = Lista.Primeiro;
+    Apontador atual = anterior->Proximo;
+    while (atual != NULL && atual->Item.ID != Item.ID)
+    {
+        anterior = atual;
+        atual = atual->Proximo;
+    }
+
+    if (atual == NULL)
+    {
+        cout << "Elemento " << Item.ID << " nao encontrado." << endl;
+        return;
+    }
+
+    anterior->Proximo = atual->Proximo;
+    if (atual == Lista.Ultimo)
+        Lista.Ultimo = anterior;
+    else
+        atual->Proximo->Anterior = anterior;
+
+    cout << "Elemento Removido = " << atual->Item.ID << endl;
+    delete atual;
+}
+//--------------------------------------------
+void Imprime(TLista Lista)
+{
+    cout << "Itens na LISTA: " << endl;
+    if (Vazia(Lista))
+    {
+        cout << "Lista esta vazia." << endl;
+    }
+    else
+    {
+        Apontador aux = Lista.Primeiro->Proximo;
+        while (aux != NULL)
+        {
+            cout << aux->Item.ID << " ";
+            aux = aux->Proximo;
+        }
+        cout << endl;
+    }
+}
+//--------------------------------------------
+void CarregaArquivo(TLista &Lista, const string nomeArquivo)
+{
+    ifstream arquivo(nomeArquivo);
+    if(!arquivo.is_open()) {
+        cerr << "Erro ao abrir o arquivo." << endl;
+        return;
+    }
+    
+    string linha;
+    while(getline(arquivo, linha)) {
+        stringstream ss(linha);
+        string campo;
+        TInfo item;
+        
+        getline(ss, campo, ';');
+        item.ID = stoi(campo);
+        
+        getline(ss, campo, ';');
+        item.Cidade = campo;
+        
+        getline(ss, campo, ';');
+        item.Rodovia = campo;
+        
+        getline(ss, campo, ';');
+        item.Data = campo;
+        
+        getline(ss, campo, ';');
+        item.DiaSemana = campo;
+        
+        getline(ss, campo, ';');
+        item.Hora = campo;
+        
+        getline(ss, campo, ';');
+        item.TipoAcidente = campo;
+        
+        getline(ss, campo, ';');
+        item.Feridos = stoi(campo);
+        
+        getline(ss, campo, ';');
+        item.Mortos = stoi(campo);
+        
+        getline(ss, campo, ';');
+        item.Descricao = campo;
+        
+        InsereFinal(Lista, item);
+    }
+    arquivo.close();
+}
+//--------------------------------------------
+void ImprimeAcidente(TLista &Lista, int ID)
+{
+    Apontador aux = Lista.Primeiro->Proximo;
+    bool encontrado = false;
+    while (aux != NULL)
+    {
+        if(aux->Item.ID == ID) {
+            encontrado = true;
+            cout << "ID: " << aux->Item.ID << endl;
+            cout << "Cidade: " << aux->Item.Cidade << endl;
+            cout << "Rodovia: " << aux->Item.Rodovia << endl;
+            cout << "Data: " << aux->Item.Data << endl;
+            cout << "Dia da Semana: " << aux->Item.DiaSemana << endl;
+            cout << "Hora: " << aux->Item.Hora << endl;
+            cout << "Tipo de Acidente: " << aux->Item.TipoAcidente << endl;
+            cout << "Feridos: " << aux->Item.Feridos << endl;
+            cout << "Mortos: " << aux->Item.Mortos << endl;
+            cout << "Descricao: " << aux->Item.Descricao << endl;
+            break;
+        }
+        aux = aux->Proximo;
+    }
+    if(!encontrado) {
+        cout << "Acidente com ID " << ID << " nao encontrado." << endl;
+    }
+}
+//--------------------------------------------
+void ImprimePorID(TLista &Lista, int ID)
+{
+    ImprimeAcidente(Lista, ID);
+}
+//--------------------------------------------
+
---
+++ lista_apontador_dupla/LISTA_APONTADOR_DUPLA.h
@@ -0,0 +1,43 @@
+#pragma once
+#include <string>
+
+using namespace std;
+
+typedef struct NodoLista *Apontador;
+typedef struct
+{
+    int ID;
+    string Cidade;
+    string Rodovia;
+    string Data;
+    string DiaSemana;
+    string Hora;
+    string TipoAcidente;
+    int Feridos;
+    int Mortos;
+    string Descricao;
+} TInfo;
+struct NodoLista
+{
+    TInfo Item;
+    struct NodoLista *Anterior;
+    struct NodoLista *Proximo;
+};
+typedef struct
+{
+    struct NodoLista *Primeiro;
+    struct NodoLista *Ultimo;
+} TLista;
+
+void Cria(TLista &Lista);
+bool Vazia(TLista Lista);
+void InsereInicio(TLista &Lista, TInfo Item);
+void InserePosicao(TLista &Lista, int pos, TInfo Item);
+void InsereFinal(TLista &Lista, TInfo Item);
+void RemoveInicio(TLista &Lista);
+void RemoveFinal(TLista &Lista);
+void PesquisaRemove(TLista &Lista, TInfo Item);
+void Imprime(TLista Lista);
+void CarregaArquivo(TLista &Lista, const string nomeArquivo);
+void ImprimeAcidente(TLista &Lista, int ID);
+void ImprimePorID(TLista &Lista, int ID);
